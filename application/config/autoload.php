5b8b2db1e67c74fb1c38886e5eadaef0fae2efe6
LARDJANE <halim.lardjane@gmail.com>
2017-02-06 17:50:57 -0500

    config the session

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,9 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('session');
+
+
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +91,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url', 'security');
 
 /*
 | -------------------------------------------------------------------
--- application/config/config.php
+++ application/config/config.php
@@ -136,7 +136,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = FALSE;
+$config['composer_autoload'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '@SDmx}};FS789@#$5-098q11!@)(^45';
 
 /*
 |--------------------------------------------------------------------------
