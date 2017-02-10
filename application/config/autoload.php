3b61c51e02b0145a2dc06b1f9418f65b280841b0
LARDJANE <halim.lardjane@gmail.com>
2017-02-10 16:27:03 -0500

    working on google auth

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,8 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'facebook');
-
+$autoload['libraries'] = array('session', 'database', 'facebook');
 
 
 /*
---
+++ application/controllers/Google.php
@@ -0,0 +1,86 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User_Authentication extends CI_Controller {
+
+    /*
+     * http://www.codexworld.com/login-with-google-account-in-codeigniter/
+     */
+    function __construct() {
+        parent::__construct();
+        // Load user model
+        $this->load->model('user');
+    }
+
+    /*
+     * 
+     */
+    public function index() {
+        // Include the google api php libraries
+        include_once APPPATH . "libraries/google-api-php-client/Google_Client.php";
+        include_once APPPATH . "libraries/google-api-php-client/contrib/Google_Oauth2Service.php";
+
+        // Google Project API Credentials
+        $clientId = 'Insert Google Client ID';
+        $clientSecret = 'Insert Google Client secret';
+        $redirectUrl = base_url() . 'google/';
+
+        // Google Client Configuration
+        $gClient = new Google_Client();
+        $gClient->setApplicationName('Login to codexworld.com');
+        $gClient->setClientId($clientId);
+        $gClient->setClientSecret($clientSecret);
+        $gClient->setRedirectUri($redirectUrl);
+        $google_oauthV2 = new Google_Oauth2Service($gClient);
+
+        if (isset($_REQUEST['code'])) {
+            $gClient->authenticate();
+            $this->session->set_userdata('token', $gClient->getAccessToken());
+            redirect($redirectUrl);
+        }
+
+        $token = $this->session->userdata('token');
+        if (!empty($token)) {
+            $gClient->setAccessToken($token);
+        }
+
+        if ($gClient->getAccessToken()) {
+            $userProfile = $google_oauthV2->userinfo->get();
+            // Preparing data for database insertion
+            $userData['oauth_provider'] = 'google';
+            $userData['oauth_uid'] = $userProfile['id'];
+            $userData['first_name'] = $userProfile['given_name'];
+            $userData['last_name'] = $userProfile['family_name'];
+            $userData['email'] = $userProfile['email'];
+            $userData['gender'] = $userProfile['gender'];
+            $userData['locale'] = $userProfile['locale'];
+            $userData['profile_url'] = $userProfile['link'];
+            $userData['picture_url'] = $userProfile['picture'];
+            // Insert or update user data
+            $userID = $this->user->checkUser($userData);
+            if (!empty($userID)) {
+                $data['userData'] = $userData;
+                $this->session->set_userdata('userData', $userData);
+            } 
+            else {
+                $data['userData'] = array();
+            }
+        } 
+        else {
+            $data['authUrl'] = $gClient->createAuthUrl();
+        }
+        $this->load->view('user_authentication/index', $data);
+    }
+
+    /*
+     * 
+     */
+    public function logout() {
+        $this->session->unset_userdata('token');
+        $this->session->unset_userdata('userData');
+        $this->session->sess_destroy();
+        redirect('/google');
+    }
+
+}
